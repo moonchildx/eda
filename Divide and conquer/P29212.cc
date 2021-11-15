4797e21d830fa319105d95d597376ee6c88fcd03
Aina <93551599+moonchildx@users.noreply.github.com>
2021-11-15 12:48:51 +0100

    Update P29212.cc

--- Divide and conquer/P29212.cc
+++ Divide and conquer/P29212.cc
@@ -1,15 +1,19 @@
+/* Creator: Aina Gomez
+*/
 
 #include <iostream>
 using namespace std;
 
+// funcio recursiva que calcula b^e mod m
+// per obtenir la manera mes eficient, a cada crida calculem el mod de m.
 int calcular_exp_modm(int b, int e, int m) {
     
-    if (e == 0) return 1;
-    else if (e%2 == 0) {
+    if (e == 0) return 1;                       // cas base: exponent = 0.
+    else if (e%2 == 0) {                        // en cas que l'exponent es parell, podem simplificar l'operacio com b^(e/2) * b^(e/2) (= b^e).
         int y = calcular_exp_modm(b, e/2, m)%m;
         return (y*y)%m;
     }
-    else {
+    else {                                      // en cas que l'exponent es imparell, podem simplificar l'operacio com b^(e/2) * b^(e/2) * b (= b^e).
         int y = calcular_exp_modm(b, e/2, m)%m;
         return ((b*y)%m*y)%m;
     }
